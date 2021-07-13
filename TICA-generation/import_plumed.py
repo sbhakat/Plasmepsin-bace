8a5efddf2893d10a3ea4e2a58a1f001c7a25688a
sbhakat <33577253+sbhakat@users.noreply.github.com>
2021-07-13 10:52:20 -0500

    Create import_plumed.py

---
+++ TICA-generation/import_plumed.py
@@ -0,0 +1,29 @@
+#Execute like python importing_plumed.py >tic{TICnumber}.dat
+from tica_metadynamics.plumed_writer import render_tica_plumed_file
+from msmbuilder.featurizer import DihedralFeaturizer
+import pandas as pd 
+import mdtraj as md 
+from msmbuilder.utils import load
+#python imports
+import os,glob
+import numpy as np
+import pickle
+
+tica_model = load("./tica_mdl_flapchi1angle.pkl")
+
+#a = np.arange(1027,1357)
+a = np.arange(1118,1276)
+top = md.load("prot.pdb", atom_indices=a)
+
+# swap this for whatever you have. The code for now supports contacts, dihedral, and angles. 
+feat = DihedralFeaturizer(types=['chi1', 'chi2'])
+
+# this basically maps every feature to atom indices. 
+df1 = pd.DataFrame(feat.describe_features(top))
+#print(df1)
+
+output  = render_tica_plumed_file(tica_mdl= tica_model, df= df1, n_tics= 5, multiple_tics=None)
+#Printing TIC1
+#print(output[0])
+#Printing TIC1
+print(output[0])
